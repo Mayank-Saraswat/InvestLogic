fe3e06069e70b65979c56a53ec5ec7eb2ceb82ef
Mayank Saraswat <mayanksaraswat497@gmail.com>
2023-03-29 16:59:11 +0530

    Optimised

--- Services/calculator.js
+++ Services/calculator.js
@@ -4,33 +4,35 @@ async function calculate(obj) {
     const investmentPeriod = Number(obj.investmentPeriod)
     const rate = (Number(obj.rateOfReturn) - Number(obj.rateOfInflation)) / 12
     const months = Number(obj.investmentPeriod) * 12;
-    const totalMonthlyInvest = monthlyInvestment*investmentPeriod;
-    let sipGrowthResult = 0;
+    const totalMonthlyInvest = monthlyInvestment*investmentPeriod*12;
+    let sipGrowthResultFinal = 0, sipGrowthResult=0;
 
-    const graph = [{
-      year: 0,
-      investment: 0,
-      value: 0,
-    }]
+    const graph = [
+      {
+        year:0,
+        investment:0,
+        value:0
+      }
+    ];
 
     for (let i = 1; i <= months; i++) {
-      sipGrowthResult += Number(obj.monthlyInvestment) * Math.pow(1 + rate / 100, i)
+      sipGrowthResult += monthlyInvestment * Math.pow(1 + rate / 100, i)
       if (i % 12 == 0) {
         const obj1 = {
           year: i / 12,
-          investment: Number(obj.monthlyInvestment) * i,
+          investment: monthlyInvestment * i,
           value: Math.round(sipGrowthResult)
         }
         graph.push(obj1)
       }
     }
     
-    sipGrowthResult = sipGrowthResult.toFixed(0);
-    const potentialCapitalGain = (obj.monthlyInvestment*obj.investmentPeriod);
+    sipGrowthResultFinal = sipGrowthResult.toFixed(0);
+    const potentialCapitalGain = sipGrowthResultFinal-totalMonthlyInvest;
 
-    graphData = {
+    const graphData = {
       graph,
-      sipGrowthResult,
+      sipGrowthResultFinal,
       monthlyInvestment,
       investmentPeriod,
       totalMonthlyInvest,
